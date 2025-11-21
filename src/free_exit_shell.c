3dd6f5628801d0a93ded96328401af0fc3afdc69
Pierre Bride <pbride@f4r10s15.clusters.42paris.fr>
2025-11-21 13:44:57 +0100

    add free_exit_shell file with all free function for all struct from t_shell TODO pass error code, message and good exit

---
+++ src/free_exit_shell.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_exit_shell.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbride <pbride@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/21 13:02:57 by pbride            #+#    #+#             */
+/*   Updated: 2025/11/21 13:43:39 by pbride           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	free_lx_sh(t_lexer *lx)
+{
+	t_token	*tmp_tok;
+
+	while (lx->head)
+	{
+		tmp_tok = lx->head->next;
+		if (lx->head->word)
+			free(lx->head->word);
+		free(lx->head);
+		lx->head = tmp_tok;
+	}
+	if (tmp_tok)
+		free(tmp_tok);
+	free(lx);
+}
+
+void	free_cmds_sh(t_cmd *cmd)
+{
+	t_cmd	*tmp_cmd;
+	t_redir	*tmp_redir;
+
+	while (cmd)
+	{
+		tmp_cmd = cmd->next;
+		if (cmd->argv)
+			free_tab(cmd->argv);
+		while (cmd->redir)
+		{
+			tmp_redir = cmd->redir->next;
+			if (cmd->redir->file)
+				free(cmd->redir->file);
+			if (cmd->redir->tmp_file)
+				free(cmd->redir->tmp_file);
+			free(cmd->redir);
+			cmd->redir = tmp_redir;
+		}
+		if (tmp_redir)
+			free(tmp_redir);
+		free(cmd);
+		cmd = tmp_cmd;
+	}
+	if (tmp_cmd)
+		free(tmp_cmd);
+}
+
+void	free_exec_sh(t_exec *exec)
+{
+	if (exec->pipes)
+		free(exec->pipes);
+	if (exec->pids)
+		free(exec->pids);
+	free(exec);
+}
+
+void	free_rdocs_sh(t_heredoc *rdoc)
+{
+	t_heredoc	*tmp_rdoc;
+
+	while (rdoc)
+	{
+		tmp_rdoc = rdoc->next;
+		if (rdoc->content)
+			free(rdoc->content);
+		if (rdoc->delimiter)
+			free(rdoc->delimiter);
+		free(rdoc);
+		rdoc = tmp_rdoc;
+	}
+	if (tmp_rdoc)
+		free(tmp_rdoc);
+}
+
+void	free_exit_sh(t_shell *sh)
+{
+	if (!sh)
+		return ;
+	if (sh->env)
+		free_tab(sh->env);
+	if (sh->lx)
+		free_lx_sh(sh->lx);
+	if (sh->cmds_head)
+		free_cmds_sh(sh->cmds_head);
+	if (sh->exec)
+		free_exec_sh(sh->exec);
+	if (sh->rdoc)
+		free_rdocs_sh(sh->rdoc);
+}
